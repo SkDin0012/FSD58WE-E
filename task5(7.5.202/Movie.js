d29d9ae90a4868f1199dd0c3260756427a3edda6
Dinesh kumar <dineshkumar@Dineshs-MacBook-Air.local>
2024-05-05 22:49:00 +0530

    the add Movie

--- task5(7.5.202/Movie.js
+++ task5(7.5.202/Movie.js
@@ -1,23 +1,30 @@
 class Movie{
-    constructor(title,studio,rating){
+    constructor(title,studio,rating="PG"){
         this.title=title;
         this.studio=studio;
         this.rating=rating;
 
     }
-    PG(Movie){
-        this.rating=rating||"PG";
-        return Movie.filter(movie=>movie.rating==="PG");
-    }
-    getPG(){
-        return "Rating" +this.rating;
-    }
+   static getPG(Movie){
+      return Movie.filter(Movie=>Movie.rating==="PG");
+   }
     toString(){
         console.log(`Titile: ${this.title},\nStudio:${this.studio},\nRating:${this.rating}`);
     }
 }
 const MovieTime=new Movie("Casino Royale","Eon Productions","PG­13");
 console.log(MovieTime.toString());
+
+const Moviepg=[
+    new Movie("LEO","Seven Screen studio","PG"),
+    new Movie("The Spiderman"," Walt Disney","PG"),
+    new Movie("HULK","Marvel",'PG'),
+    new Movie("2012","Sony Pictures","PG-13")
+]
+
+const MovieResult=Movie.getPG(Moviepg);
+console.log("The result",MovieResult);
+
 
 
 
